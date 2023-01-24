92e84acf24ef38822763bc9f17794c281b344cc3
Eric Tyler <83565207+tylereric24@users.noreply.github.com>
2023-01-24 14:51:02 -0800

    Update turtleVisitor.py

--- turtleVisitor.py
+++ turtleVisitor.py
@@ -23,39 +23,56 @@ class turtleVisitor(ParseTreeVisitor):
 
     # Visit a parse tree produced by turtleParser#drawlineExpr.
     def visitDrawlineExpr(self, ctx:turtleParser.DrawlineExprContext):
+     #get target x and y coordinates
      target_x = int(ctx.x_cord.text)
      target_y = int(ctx.y_cord.text)
+     #get current position
      cur_pos = turty.pos()
+     #set background color
      bgcolor('black')
      x = 1
      y = 75
+     #move turtle to the home position
      turty.home()
+     #penup turtle
      turty.penup()
+     #set turtle speed
      turty.speed(10)
+     #move turtle to the target position
      turty.goto(target_x, target_y)
+     #pendown turtle
      turty.pendown()
+     #if target y is greater than or equal to current y position
      if target_y >= cur_pos[0]:
         while x < 250:
           r = randint(0, 255)
           g = randint(0, 255)
           b = randint(0, 255)
-         
-           
+          #set turtle shape
           turty.shape("turtle")
+          #pendown turtle
           turty.pendown()
+          #set turtle speed
           turty.speed(5000)
+          #set color mode
           colormode(255)
+          #set pen size
           turty.pensize(y)
+          #set pen color
           turty.pencolor(r,g,b)
+          #move turtle forward
           turty.forward(40 + x)
+          #rotate turtle
           turty.right(90.99)
           x = x+1
           y = y-0.3
      else:
          while x < 300:
+          #assigns random value to red green and blue for color values
           r = randint(0, 255)
           g = randint(0, 255)
           b = randint(0, 255)
+          #set turtle shape
           turty.shape("turtle")
           turty.pendown()
           turty.speed(5000)
